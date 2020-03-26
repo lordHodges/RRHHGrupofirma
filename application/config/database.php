248954b0e28d6196e08a184d776018c0c8119c8a
Caterin Morales <caterin.morales5587@gmail.com>
2020-03-26 16:14:28 -0300

    SPRINT 3: Contratos - ver listado de contratos listos.

--- application/config/database.php
+++ application/config/database.php
@@ -26,27 +26,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-
-// PRODUCTION
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'localhost',
-// 	'username' => 'tudesarr_cmorales',
-// 	'password' => 'knowledgeispower23',
-// 	'database' => 'tudesarr_fa_rrhh',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
-
