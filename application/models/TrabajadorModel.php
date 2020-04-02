7532d1ccc9584f433a32a8b1e906a783e698bc77
Caterin Morales <caterin.morales5587@gmail.com>
2020-04-02 10:46:25 -0300

    Corrección de editar trabajador

--- application/models/TrabajadorModel.php
+++ application/models/TrabajadorModel.php
@@ -254,15 +254,15 @@ class TrabajadorModel extends CI_Model {
       $this->db->where('t.cp_trabajador', $idTrabajador);
       $resultado =  $this->db->update("fa_trabajador t", $dataTrabajador);
 
-      var_dump("ID: ",$idTrabajador);
-      var_dump("DATA TRABAJADOR: ",$dataTrabajador);
+      // var_dump("ID: ",$idTrabajador);
+      // var_dump("DATA TRABAJADOR: ",$dataTrabajador);
 
       return $resultado;
-      // if($resultado){
-      //   return "ok";
-      // }else{
-      //   return "error";
-      // }
+      if($resultado){
+        return "ok";
+      }else{
+        return "error";
+      }
   }
 
 
