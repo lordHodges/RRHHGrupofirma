a6252f3d050197f3fae34ff7a142be1a1c1e758a
Caterin Morales <caterin.morales5587@gmail.com>
2020-04-02 11:04:26 -0300

    Corrección de editar trabajador

--- assets/js/trabajador.js
+++ assets/js/trabajador.js
@@ -1,7 +1,7 @@
 /*************************** TRABAJADOR ****************************/
 var base_url = 'http://10.10.11.240/RRHH-FIRMA/index.php/';
 
-cargarTablaTrabajador(){
+function cargarTablaTrabajador(){
   var table = $('#tabla_trabajador').DataTable();
   table.destroy();
 
