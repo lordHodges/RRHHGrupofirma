e74da70c88ff16f95bdf67a9dfeaec5c19812e9c
Caterin Morales <caterin.morales5587@gmail.com>
2020-05-20 11:35:03 -0400

    editar adelanto

--- application/models/AdelantosModel.php
+++ application/models/AdelantosModel.php
@@ -97,6 +97,8 @@ class AdelantosModel extends CI_Model {
             "cf_banco"             => $banco
         );
 
+
+
         $this->db->where('cp_adelanto', $idAdelanto);
         $resultado =  $this->db->update("fa_adelanto", $data);
 
