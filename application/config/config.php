026013e4db62f8c81c48af161b57ab908bb49ed0
Caterin Morales <caterin.morales5587@gmail.com>
2020-05-23 16:05:26 -0400

    .

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://10.10.10.1/grupofirma/';
+$config['base_url'] = 'http://imlchile.cl/grupofirma/';
 
 
 /*
