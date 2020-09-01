31366115c252bf719dfeb674281407327fde4e5b
Victor Hodges <hodgestroncoso@gmail.com>
2020-09-01 18:51:15 -0400

    repair white spaces

--- application/libraries/Pdfgenerator.php
+++ application/libraries/Pdfgenerator.php
@@ -19,7 +19,7 @@ class pdfgenerator
         $dompdf->render();
         if ($stream) {
             // "Attachment" => 1 hará que por defecto los PDF se descarguen en lugar de presentarse en pantalla.
-			ob_flush();
+			/* ob_flush(); */
 			$dompdf->stream($filename.".pdf", array("Attachment" => $download));
 			
         } else {
