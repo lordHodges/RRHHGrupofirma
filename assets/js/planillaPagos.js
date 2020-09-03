8d18740a14de019a72476c45b3b185ec3c82ee21
Victor Hodges <hodgestroncoso@gmail.com>
2020-09-03 19:03:14 -0400

    agrgar export tabla en dom

--- assets/js/planillaPagos.js
+++ assets/js/planillaPagos.js
@@ -252,7 +252,58 @@ function cargarTablaPagosFinDeMes() {
 			},
 		],
 		dom: '<"html5buttons"B>lTfgitp',
-		buttons: [],
+		buttons: [
+			{
+				extend: "copy",
+				exportOptions: {
+					columns: [1, 2, 3, 4, 5, 6, 7],
+				},
+			},
+			{
+				extend: "csv",
+				exportOptions: {
+					columns: [1, 2, 3, 4, 5, 6, 7],
+				},
+			},
+			{
+				extend: "excel",
+				title: "Lista de Trabajadores",
+				exportOptions: {
+					columns: [1, 2, 3, 4, 5, 6, 7],
+				},
+			},
+			{
+				extend: "pdf",
+				title: "Lista de Trabajadores",
+				exportOptions: {
+					columns: [1, 2, 3, 4, 5, 6, 7],
+				},
+				customize: function (doc) {
+					doc.styles.title = {
+						fontSize: "25",
+						alignment: "center",
+					};
+					doc.styles["td:nth-child(2)"] = {
+						padding: "100px",
+					};
+				},
+			},
+			{
+				extend: "print",
+				title: "Grupo Firma",
+				exportOptions: {
+					columns: [1, 2, 3, 4, 5, 6, 7],
+				},
+				customize: function (win) {
+					$(win.document.body).addClass("white-bg");
+					$(win.document.body).css("font-size", "10px");
+					$(win.document.body)
+						.find("table")
+						.addClass("compact")
+						.css("font-size", "inherit");
+				},
+			},
+		],
 		lengthMenu: [
 			[100, 50, 25, -1],
 			[100, 50, 25, "All"],
