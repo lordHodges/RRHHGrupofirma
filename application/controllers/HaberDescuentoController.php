a74704d4ae80738d1b33def3c5abab4e1627c88c
Simón Faúndez Torres <simon.faundezt@gmail.com>
2021-01-13 14:38:01 -0300

    Haberes y Descuentos - Variable

--- application/controllers/HaberDescuentoController.php
+++ application/controllers/HaberDescuentoController.php
@@ -12,7 +12,7 @@ class HaberDescuentoController extends CI_Controller {
     public function index(){
 
         $this->load->view('template/menu');
-        $this->load->view('mantenedores/haberdescuento');
+        $this->load->view('mantenedores/haberDescuento');
     }
 
     public function getListadoHaberDescuento(){
